79cb1f0d4bcd5e13bfdf6ffd0c815e8c26062ea0
Farid Khan <farid_khan75@outlook.com>
2025-11-18 16:43:44 +0000

    basic movement added to controller

--- controllers/social_robot/social_robot.py
+++ controllers/social_robot/social_robot.py
@@ -5,5 +5,16 @@ TIME_STEP = int(robot.getBasicTimeStep())
 
 print("[INFO] Social Python Controller Initialized with TIME_STEP =", TIME_STEP)
 
+# Initialise motors
+left_motor = robot.getDevice('left wheel')
+right_motor = robot.getDevice('right wheel')
+
+left_motor.setPosition(float('inf'))
+right_motor.setPosition(float('inf'))   
+
+forward_speed = 2.0 # Forward speed in rad/s
+left_motor.setVelocity(forward_speed)
+right_motor.setVelocity(forward_speed)
+
 while robot.step(TIME_STEP) != -1:
     pass
