1c56bd0a63bf64fbb516427506ea3de7dc0d58b6
Farid Khan <farid_khan75@outlook.com>
2025-11-18 16:52:18 +0000

    Added Wheel Encoder to controller

--- controllers/social_robot/social_robot.py
+++ controllers/social_robot/social_robot.py
@@ -5,6 +5,7 @@ TIME_STEP = int(robot.getBasicTimeStep())
 
 print("[INFO] Social Python Controller Initialized with TIME_STEP =", TIME_STEP)
 
+# --------------- Robot Motors Setup -----------------
 # Initialise motors
 left_motor = robot.getDevice('left wheel')
 right_motor = robot.getDevice('right wheel')
@@ -16,5 +17,20 @@ forward_speed = 2.0 # Forward speed in rad/s
 left_motor.setVelocity(forward_speed)
 right_motor.setVelocity(forward_speed)
 
+# --------------- Robot Encoders Setup -----------------
+# Initialise encoders
+left_encoder = robot.getPositionSensor('left wheel sensor')
+right_encoder = robot.getPositionSensor('right wheel sensor')
+
+left_encoder.enable(TIME_STEP)
+right_encoder.enable(TIME_STEP)
+
+
+
 while robot.step(TIME_STEP) != -1:
-    pass
+    left_val = left_encoder.getValue()
+    right_val = right_encoder.getValue()
+
+    t = robot.getTime()
+    if int(t*2) % 2 == 0:
+        print(f"[TIME {t:.2f} s] Left Encoder: {left_val:.4f} rad, Right Encoder: {right_val:.4f} rad")
